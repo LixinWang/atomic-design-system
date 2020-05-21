235d67bb2132dea952d27bcf3781f51ef6d0a812
Lixin Wang <lixinw1108@gmail.com>
2020-05-21 16:29:55 -0700

    Create header.js and its css file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Atomic Design System',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,21 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
+
+//Alternative way to import aside from the single line require in line 9
+//    import logo from '../images/Icons/Leaf.svg'
+//    <img src={logo} width="30" />
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/"><img src={require
+        ('../images/Icons/Leaf.svg')}
+        width="30" /></Link>
+      <Link to="/page2">Page 2</Link>
+      <Link to="/page3">Page 3</Link>
+      <Link to="/page4">Page 4</Link>
+      <Link to="/page5"><button>Button</button></Link>
     </div>
   </div>
 )
