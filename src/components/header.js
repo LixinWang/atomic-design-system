fb2ef0a12eef6d6acf0bfdc8f70c32a0a04531f2
Lixin Wang <lixinw1108@gmail.com>
2020-06-01 12:20:04 -0700

    Create stateful header componenet

--- src/components/header.js
+++ src/components/header.js
@@ -6,18 +6,45 @@ import './Header.css'
 //    import logo from '../images/Icons/Leaf.svg'
 //    <img src={logo} width="30" />
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/"><img src={require
-        ('../images/Icons/Leaf.svg')}
-        width="30" /></Link>
-      <Link to="/atoms">Atoms</Link>
-      <Link to="/404">Molecules</Link>
-      <Link to="/404">Organisms</Link>
-      <Link to="/404"><button>Button</button></Link>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  // Listen for scroll
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/"><img src={require
+            ('../images/Icons/Leaf.svg')}
+            width="30"/></Link>
+          <Link to="/atoms">Atoms</Link>
+          <Link to="/404">Molecules</Link>
+          <Link to="/404">Organisms</Link>
+          <Link to="/404"><button>Button</button></Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
